819a8698d361c10e31cb3e67c45d110dbc2b55ab
Kin Chan <xkinchanx@gmail.com>
2018-08-14 16:16:12 -0700

    [feat] home base displaying in app with scrollview

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+import Base from './Pages/Base/Base';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div id="App" className="flexCenter">
+        <Base />
+      </div>
+    );
+  }
+}
+
+export default App;
