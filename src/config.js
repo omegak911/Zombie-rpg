b9f2ac0be99848b2b6ccf7017c7274243f5022ee
Kin Chan <xkinchanx@gmail.com>
2018-08-14 16:16:39 -0700

    [feat] sprite assets and config for dynamic changes

---
+++ src/config.js
@@ -0,0 +1,13 @@
+import playerImage from './assets/sprites/hero3.png';
+import npcImage from './assets/sprites/npc3.png';
+
+const characterConfigs = {
+  player: {
+    backgroundImage: playerImage,
+  },
+  npc: {
+    backgroundImage: npcImage,
+  }
+}
+
+export default characterConfigs;
