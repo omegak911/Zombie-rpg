a25796fc263842a568c11c453bfa8317d01b97e1
Kin Chan <xkinchanx@gmail.com>
2018-08-14 18:41:45 -0700

    [prog] on mount, view will attempt to center player on screen

--- src/Pages/Base/Base.js
+++ src/Pages/Base/Base.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 import CharacterModel from '../../Components/CharacterModel/CharacterModel';
+import { mapConfigs, movementConfigs } from '../../config';
 import './Base.css';
 
 class Base extends Component {
@@ -30,19 +31,43 @@ class Base extends Component {
 
   componentDidMount() {
     //configurations to baseMatrix here based on existing buildings
-    const homebase = document.getElementById('homebase');
-    homebase.style.visibility = 'hidden';
-    homebase.scrollLeft = 315;
-    setTimeout(() => {
-      homebase.scrollTop = 120;
-      homebase.style.visibility = 'visible';
-    }, 500);
+
+    this.centerInitialViewOnPlayer();
   }
 
-
+  //view will auto-scroll depending on player position
   autoScroll = () => {
     const homebase = document.getElementById('homebase');
-    console.dir(homebase);
+    const player = document.getElementsByClassName('player')[0];
+    let { offsetLeft, offsetTop } = player;
+    let { scrollLeft, scrollTop } = homebase;
+    if (offsetLeft - scrollLeft > 340) { //move right
+      homebase.scrollLeft += movementConfigs.horizontal;
+    }
+    if (offsetTop - scrollTop > 195) { //move down
+      homebase.scrollTop += movementConfigs.vertical;
+    }
+    if (offsetLeft - scrollLeft < 300) { //move left
+      homebase.scrollLeft -= movementConfigs.horizontal;
+    }
+    if (offsetTop - scrollTop < 160) { //move up
+      homebase.scrollTop -= movementConfigs.vertical;
+    }
+  }
+
+  centerInitialViewOnPlayer = () => {
+    const homebase = document.getElementById('homebase');
+    let mapMidpointX = Math.floor(mapConfigs.width/2);
+    let mapMidpointY = Math.floor(mapConfigs.height/2);
+    let viewMidpointX = Math.floor(homebase.clientWidth/2);
+    let viewMidpointY = Math.floor(homebase.clientHeight/2);
+
+    homebase.style.visibility = 'hidden';
+    homebase.scrollLeft = mapMidpointX - viewMidpointX;
+    setTimeout(() => {
+      homebase.scrollTop = mapMidpointY - viewMidpointY - mapConfigs.startPositionAdjustment.midUpperCenter;
+      homebase.style.visibility = 'visible';
+    }, 500);
   }
 
   render() {
