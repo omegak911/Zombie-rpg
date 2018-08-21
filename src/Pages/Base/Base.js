f70cc058623c900221340b84f47a67b92d0993c8
Kin Chan <xkinchanx@gmail.com>
2018-08-20 20:49:31 -0700

    [refactor] moved some code to config so we can make base more dynamic.  Also improved autoScroll to work properly at edges of map

--- src/Pages/Base/Base.js
+++ src/Pages/Base/Base.js
@@ -1,52 +1,40 @@
 import React, { Component } from 'react';
 
 import CharacterModel from '../../Components/CharacterModel/CharacterModel';
-import { characterConfigs, mapConfigs, movementConfigs } from '../../config';
+import { characterConfigs, homeBaseConfigs, mapConfigs, movementConfigs } from '../../config';
 import './Base.css';
 
 class Base extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      baseMatrix: [
-        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-        [0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0],
-        [0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0],
-        [0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0],
-        [0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0],
-        [0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0],
-        [0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0],
-        [0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0],
-        [0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0],
-        [0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0],
-        [0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0],
-        [0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0],
-        [0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0],
-        [0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0],
-        [0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0],
-        [0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0],
-        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
-      ]
+      baseMatrix: mapConfigs.matrix
     }
   }
 
-  componentDidMount() {
-    //configurations to baseMatrix here based on existing buildings
-
-    this.centerInitialViewOnPlayer();
+  async componentDidMount() {
+    //configurations to baseMatrix here based on existing buildings/signs
+    let { signCoordinates } = homeBaseConfigs;
+    let temp = await mapConfigs.matrix.slice();
+    for (let i = 0; i < signCoordinates.length; i++) {
+      temp[signCoordinates[i][0]][signCoordinates[i][1]] = await 0;
+    }
+    await this.setState({ baseMatrix: temp });
+    await this.centerInitialViewOnPlayer();
   }
 
   //view will auto-scroll depending on player position
   autoScroll = () => {
+    const { height, width } = mapConfigs;
     const homebase = document.getElementById('homebase');
     const player = document.getElementsByClassName('player')[0];
     let { offsetLeft, offsetTop } = player;
     let { clientWidth, clientHeight, scrollLeft, scrollTop } = homebase;    
     let playerIs = {
-      tooCloseToTop: offsetTop < (scrollTop + (clientHeight * .4)),
-      tooCloseToBottom: offsetTop + 40 > (scrollTop + clientHeight - (clientHeight * .4)),
-      tooCloseToLeft: offsetLeft < (scrollLeft + (clientWidth * .4)),
-      tooCloseToRight: offsetLeft + 40 > (scrollLeft + clientWidth - (clientWidth * .4)),
+      tooCloseToTop: (offsetTop < (scrollTop + (clientHeight * .3))) && scrollTop > 0,
+      tooCloseToBottom: (offsetTop + 40 > (scrollTop + clientHeight - (clientHeight * .3))) && scrollTop < height - clientHeight,
+      tooCloseToLeft: (offsetLeft < (scrollLeft + (clientWidth * .3))) && scrollLeft > 0,
+      tooCloseToRight: (offsetLeft + 40 > (scrollLeft + clientWidth - (clientWidth * .3))) && scrollLeft < width - clientWidth,
     }
 
     if (playerIs.tooCloseToTop) {
@@ -78,7 +66,11 @@ class Base extends Component {
     let { baseMatrix } = this.state;
     return (
       <div id="homebase" className="page">
-        <div className="homebase">
+        <div className="homebase" 
+          style={{
+            backgroundImage: `url(${homeBaseConfigs.backgroundImage})`,
+          }}
+          >
           <CharacterModel autoScroll={this.autoScroll} baseMatrix={baseMatrix} characterType="player"/>
           <CharacterModel baseMatrix={baseMatrix} characterType="npcMan" startCoord={{ startX: 21, startY: 10, top: 400, left: 840 }}/>
           <CharacterModel baseMatrix={baseMatrix} characterType="npcWoman" startCoord={{ startX: 28, startY: 7, top: 280, left: 1120 }}/>
