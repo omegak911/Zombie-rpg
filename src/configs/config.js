fe53303710cd9f83d5cb4a1b36b7ae5d9be633aa
Kin Chan <xkinchanx@gmail.com>
2018-08-29 22:42:36 -0700

    [prog] configs for worldMap

--- src/configs/config.js
+++ src/configs/config.js
@@ -2,12 +2,14 @@ import characterConfigs from './characterConfig';
 import homeBaseConfigs from './homeBaseConfig';
 import mapConfigs from './mapConfig.js';
 import movementConfigs from './movementConfig';
+import worldMapConfigs from './worldMapConfig';
 
 const configs = {
   characterConfigs,
   movementConfigs,
   mapConfigs,
   homeBaseConfigs,
+  worldMapConfigs,
 }
 
 //scalable width height will probably be based on length/width of matrix and current number of pixels.  
---
+++ src/configs/worldMapConfig.js
@@ -0,0 +1,69 @@
+import backgroundImage from '../assets/maps/WorldMap.png';
+
+const worldMapConfig = {
+  backgroundImage,
+  levelsAndPositions: [
+    {
+      level: 1,
+      top: 0,
+      left: 0,
+    },
+    {
+      level: 2,
+      top: 0,
+      left: 0,
+    },
+    {
+      level: 3,
+      top: 0,
+      left: 0,
+    },
+    {
+      level: 4,
+      top: 0,
+      left: 0,
+    },
+    {
+      level: 5,
+      top: 0,
+      left: 0,
+    },
+    {
+      level: 6,
+      top: 0,
+      left: 0,
+    },
+    {
+      level: 7,
+      top: 0,
+      left: 0,
+    },
+    {
+      level: 8,
+      top: 0,
+      left: 0,
+    },
+    {
+      level: 9,
+      top: 0,
+      left: 0,
+    },
+    {
+      level: 10,
+      top: 0,
+      left: 0,
+    },
+    {
+      level: 11,
+      top: 0,
+      left: 0,
+    },
+    {
+      level: 12,
+      top: 0,
+      left: 0,
+    },
+  ]
+}
+
+export default worldMapConfig;
