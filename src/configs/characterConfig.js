17f95012b63c1bffcc9b85fab3a774fa161395ef
Kin Chan <xkinchanx@gmail.com>
2018-09-02 15:49:11 -0700

    [feat] added monster sprites and background positions in config

--- src/configs/characterConfig.js
+++ src/configs/characterConfig.js
@@ -1,5 +1,6 @@
 import playerImage from '../assets/sprites/hero3.png';
 import npcImage from '../assets/sprites/npc3.png';
+import monsterImage from '../assets/sprites/monsters.png';
 
 const characterConfigs = {
   player: {
@@ -25,7 +26,32 @@ const characterConfigs = {
   npcBoy: {
       backgroundImage: npcImage,
       spriteCrop: ['0px -125px','-120px -125px','-80px -125px','-40px -125px'],
-  }
+  },
+  littleZombie: {
+    backgroundImage: monsterImage,
+    spriteCrop: ['0px 0px','-120px 0px','-80px 0px','-40px 0px']
+  },
+  regularZombie: {
+    backgroundImage: monsterImage,
+    spriteCrop: ['0px -40px','-120px -40px','-80px -40px','-40px -40px'],
+  },
+  bruteZombie: {
+    backgroundImage: monsterImage,
+    spriteCrop: ['0px -80px','-120px -80px','-80px -80px','-40px -80px'],
+  },
+  warriorZombie: {
+    backgroundImage: monsterImage,
+    spriteCrop: ['0px -200px','-120px -200px','-80px -200px','-40px -200px'],
+  },
+  redSkeleton: {
+    backgroundImage: monsterImage,
+    spriteCrop: ['0px -120px','-120px -120px','-80px -120px','-40px -120px'],
+  },
+  regularSkeleton: {
+    backgroundImage: monsterImage,
+    spriteCrop: ['0px -160px','-120px -160px','-80px -160px','-40px -160px'],
+  },
+  
 }
 
 export default characterConfigs;
